5ea5f849ce81959316f0554945839be63749f15b
DamonCharlesRoberts <damon.charles.roberts@gmail.com>
2025-02-02 12:22:53 -0500

    Adding the season ingest function.

--- src/db_init.py
+++ src/db_init.py
@@ -16,6 +16,55 @@ def parse_args() -> str:
     # Return the argument.
     return args.path
 
+def seasons_table(con:db.DuckDBPyConnection) -> None:
+    """Initialize a table of the Seasons for the MLB.
+
+    Args:
+        con (duckdb.duckDBCOnnection): The connection to the database.
+    """
+    # Initialize the table.
+    con.execute(
+        """
+        create table seasons (
+            seasonId varchar(4)
+            , hasWildcard boolean
+            , preSeasonStartDate date
+            , seasonStartDate date
+            , regularSeasonStartDate date
+            , regularSeasonEndDate date
+            , seasonEndDate date
+            , offseasonStartDate date
+            , offSeasonEndDate date
+        );
+        """
+    )
+    # Insert the data into the table.
+    # - Pull the season data from the api.
+    seasons = get("seasons", params={"all":True, "sportId":1})
+    # - Convert to a tabular format.
+    df_seasons = (
+        pl.DataFrame(seasons)
+        .unnest("seasons")
+        .select([
+            "seasonId"
+            , "hasWildcard"
+            , "preSeasonStartDate"
+            , "seasonStartDate"
+            , "regularSeasonStartDate"
+            , "regularSeasonEndDate"
+            , "seasonEndDate"
+            , "offseasonStartDate"
+            , "offSeasonEndDate"
+        ])
+    )
+    # - Insert the data into the DB.
+    con.execute(
+        """
+        insert into seasons
+            select * from df_seasons;
+        """
+    )
+    
 def meta_table(con:db.DuckDBPyConnection) -> None:
     """Initialize a table with the main ID's for the database.
     
@@ -28,8 +77,51 @@ def meta_table(con:db.DuckDBPyConnection) -> None:
     schedule = get("schedule", params={"sportId":1, "season":2024})
     # Convert to a dataframe.
     df_teams = pl.DataFrame(teams).unnest("teams")
-    df_seasons = pl.DataFrame(seasons).unnest("seasons")
-    df_schedule = pl.DataFrame(schedule).select(["dates"]).unnest("schedule").explode("games").unnest("games").unnest("teams")
+    df_seasons = (
+        pl.DataFrame(seasons)
+        .unnest("seasons")
+        .select([
+            "seasonId"
+            , "hasWildcard"
+            , "preSeasonStartDate"
+            , "seasonStartDate"
+            , "regularSeasonStartDate"
+            , "regularSeasonEndDate"
+            , "seasonEndDate"
+            , "offseasonStartDate"
+            , "offSeasonEndDate"
+        ])
+    )
+    df_schedule = (
+        pl.DataFrame(schedule)
+        .select(["dates"])
+        .unnest("dates")
+        .explode("games")
+        .unnest("games")
+        .unnest("teams")
+        .with_columns(
+            pl.col("away").struct.field("team").struct.field("id").alias("away_team")
+            , pl.col("away").struct.field("leagueRecord").struct.field("wins").alias("away_team_wins")
+            , pl.col("away").struct.field("leagueRecord").struct.field("losses").alias("away_team_losses")
+            , pl.col("home").struct.field("team").struct.field("id").alias("home_team")
+            , pl.col("home").struct.field("leagueRecord").struct.field("wins").alias("home_team_wins")
+            , pl.col("home").struct.field("leagueRecord").struct.field("losses").alias("home_team_losses")
+        )
+        .select(
+            [
+                "season"
+                ,"date"
+                , "gamePk"
+                , "doubleHeader"
+                , "away_team"
+                , "away_team_wins"
+                , "away_team_losses"
+                , "home_team"
+                , "home_team_wins"
+                , "home_team_losses"
+            ]
+        )
+    )
     # Place dataframe in a table.
     con.execute(
         """
