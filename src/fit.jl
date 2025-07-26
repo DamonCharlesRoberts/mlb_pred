294175ae3a469d8ac0ac56c496f9d35bdb65caf6
DamonCharlesRoberts <dev.properly669@passmail.net>
2025-07-26 14:39:12 -0400

    Writing up the models.

--- src/fit.jl
+++ src/fit.jl
@@ -1,26 +1,46 @@
 using DataFrames
 using DuckDB
+using Turing
 
 # Get the data.
 con = DBInterface.connect(DuckDB.DB, "./data/twenty_five.db")
-df_1_pos = DataFrame(
+df = DataFrame(
     DBInterface.execute(
         con 
         , """
+        with a as (
+            select
+                scores.game_id
+                , schedule.home_team
+                , scores.home_runs
+                , schedule.away_team
+                , scores.away_runs
+            from scores
+                left join schedule
+                on scores.game_id=schedule.game_id
+            where schedule.season_id like '2025'
+        )
         select
-            sample_id
-            , wave
-            , choice_1
-            , choice_2
-            , (
-                case when preferred==choice_1 then 1
-                when preferred==choice_2 then 0
-                else null
-                end
-            ) as y
-        from responses
-        where wave like '1'
-        and pos_battery==1;
+            game_id
+            , home_team
+            , away_team
+            , (case
+                when home_runs > away_runs then 1
+                else 0
+            end) as home_win
+        from a
         """
     )
-) 
+)
+
+team_idx = sortperm(unique(append!(copy(df.home_team), copy(df.away_team))))
+
+# Define the models. 
+@model function simple(x::Array, y::Array, d::Vector)
+    α ~ filldist(truncated(Normal(0., 1.), 0., Inf), d)
+    θ = 1. ./ 1. .+ exp.(-(log.(α[x[:,1]]) .- log.(α[x[:,2]])))
+    y .~ Bernoulli.(θ)
+end
+
+mod = simple(Matrix(select(df, [:home_team, :away_team])), df.home_win, team_idx)
+fit = Turing.sample(mod, NUTS(), MCMCThreads(), 1_000, 4)
